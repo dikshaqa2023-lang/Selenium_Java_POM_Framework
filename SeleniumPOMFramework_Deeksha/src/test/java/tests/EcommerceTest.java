6ddd08ac7b54f74162e4c62db6e72b25e46d8549
dikshaqa2023-lang <dikshaqa2023@gmail.com>
2026-09-21 20:41:15 +0530

    Update EcommerceTest.java

--- SeleniumPOMFramework_Deeksha/src/test/java/tests/EcommerceTest.java
+++ SeleniumPOMFramework_Deeksha/src/test/java/tests/EcommerceTest.java
@@ -6,11 +6,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import base.BaseTest;
-import pages.CartPage;
-import pages.CheckoutPage;
 import pages.HomePage;
 import pages.LoginPage;
-import pages.ProductPage;
 
 public class EcommerceTest extends BaseTest {
     LoginPage loginPage;
