358ba7853562515ae34f980ef2c6831338a5f304
dikshaqa2023-lang <dikshaqa2023@gmail.com>
2026-09-21 20:34:40 +0530

    Update LoginPage.java

---
+++ SeleniumPOMFramework_Deeksha/src/test/java/pages/LoginPage.java
@@ -0,0 +1,53 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    WebDriver driver;
+
+    private By username = By.id("username");
+    private By password = By.id("password");
+    private By loginButton = By.id("loginBtn");
+    private By errorMessage = By.xpath("//div[@class='error-message']");
+    private By logoutButton = By.xpath("//button[text()='Logout']");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void enterUsername(String userName) {
+        driver.findElement(username).clear();
+        driver.findElement(username).sendKeys(userName);
+    }
+
+    public void enterPassword(String pass) {
+        driver.findElement(password).clear();
+        driver.findElement(password).sendKeys(pass);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginButton).click();
+    }
+
+    public HomePage login(String userName, String pass) {
+        enterUsername(userName);
+        enterPassword(pass);
+        clickLogin();
+        return new HomePage(driver);
+    }
+
+    public void invalidLogin(String userName, String pass) {
+        enterUsername(userName);
+        enterPassword(pass);
+        clickLogin();
+    }
+
+    public String getErrorMessage() {
+        return driver.findElement(errorMessage).getText();
+    }
+
+    public void logout() {
+        driver.findElement(logoutButton).click();
+    }
+}
