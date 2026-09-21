d43ebf6a60a598134f0f2ecbca4e8c1f45ecb4a6
dikshaqa2023-lang <dikshaqa2023@gmail.com>
2026-09-21 20:40:09 +0530

    Update EcommerceTest.java

---
+++ SeleniumPOMFramework_Deeksha/src/test/java/tests/EcommerceTest.java
@@ -0,0 +1,58 @@
+package tests;
+
+import java.io.IOException;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import base.BaseTest;
+import pages.CartPage;
+import pages.CheckoutPage;
+import pages.HomePage;
+import pages.LoginPage;
+import pages.ProductPage;
+
+public class EcommerceTest extends BaseTest {
+    LoginPage loginPage;
+  
+@BeforeMethod
+    public void launchApplication() throws IOException {
+        setup();
+        loginPage = new LoginPage(driver);
+    }
+
+    @Test(priority = 1)
+    public void validLoginTest() {
+        homePage = loginPage.login(
+            prop.getProperty("username"),
+            prop.getProperty("password")
+        );
+
+        Assert.assertTrue(
+            driver.getCurrentUrl().contains("home"),
+            "Login was not successful"
+        );
+    }
+
+    @Test(priority = 2)
+    public void invalidLoginTest() {
+        loginPage.invalidLogin(
+            prop.getProperty("invalidUsername"),
+            prop.getProperty("invalidPassword")
+        );
+
+        String error = loginPage.getErrorMessage();
+
+        Assert.assertTrue(
+            error.contains("Invalid"),
+            "Invalid login error message not displayed"
+        );
+      loginPage.logout();
+   
+    }
+
+   @AfterMethod
+    public void closeBrowser() {
+        tearDown();
+    }
+}
